c35f84d58c07d30336659369bb51bddffa06a8bb
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-07-07 17:14:30 +0200

    using ejs-button component

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ejs-button>Button</ejs-button>
   </div>
 </template>
 
 <script>
+import Vue from 'vue';
+import { ButtonPlugin } from '@syncfusion/ej2-vue-buttons';
 import HelloWorld from './components/HelloWorld.vue'
+
+Vue.use(ButtonPlugin);
 
 export default {
   name: 'App',
@@ -17,6 +22,9 @@ export default {
 </script>
 
 <style>
+@import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
