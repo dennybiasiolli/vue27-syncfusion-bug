7174f1c3fdfabe9334b54b5a312d42421c80b6ec
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2022-11-04 08:45:10 +0100

    adding RichTextEditorPlugin to App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,13 @@
 import Vue from 'vue';
 import { ButtonPlugin } from '@syncfusion/ej2-vue-buttons';
 import { GridPlugin } from '@syncfusion/ej2-vue-grids';
+import { RichTextEditorPlugin } from '@syncfusion/ej2-vue-richtexteditor';
 import HelloWorld from './components/HelloWorld.vue'
 import BasicList from './components/BasicList.vue'
 
 Vue.use(ButtonPlugin);
 Vue.use(GridPlugin);
+Vue.use(RichTextEditorPlugin);
 
 export default {
   name: 'App',
@@ -29,6 +31,13 @@ export default {
 <style>
 @import '../node_modules/@syncfusion/ej2-base/styles/material.css';
 @import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-lists/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-navigations/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-vue-richtexteditor/styles/material.css';
+
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
