3d8238987214933184c143b84369a3c7b5f27d8a
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-02-15 17:14:28 +0100

    WTF is happening again?

--- src/components/GridExample.vue
+++ src/components/GridExample.vue
@@ -1,5 +1,5 @@
 <template>
-  <ejs-grid ref='grid' :dataSource='data' :allowFiltering='true' :filterSettings='filteroption' height='270px'>
+  <ejs-grid ref='grid' :dataSource='data' :allowFiltering='true' :filterSettings='filteroption' height='270px' :dataStateChange="handleDataStateChange">
     <e-columns>
       <e-column field='OrderID' headerText='Order ID' textAlign='Right' width=100></e-column>
       <e-column field='EmployeeID' headerText='Employee Name' :dataSource='employeeData' foreignKeyValue='FirstName'
@@ -13,28 +13,37 @@
 <script>
 import { createElement } from '@syncfusion/ej2-base';
 import { Edit, Toolbar, ForeignKey, Filter } from "@syncfusion/ej2-vue-grids";
-import { DropDownList } from "@syncfusion/ej2-dropdowns";
+import { MultiSelect, CheckBoxSelection } from "@syncfusion/ej2-dropdowns";
 import { DataManager } from '@syncfusion/ej2-data';
 import { data, fEmployeeData } from './datasource.js';
+
+MultiSelect.Inject(CheckBoxSelection); // mandatory for `mode: 'CheckBox'`
 
 let dropInstance;
 
 export default {
   data: () => {
     return {
-      data: data,
+      data: { count: 123, result: data },
       employeeData: fEmployeeData,
       filteroption: { type: 'Menu' },
       filter: {
+        operator: "equal",
         ui: {
           create: (args) => {
             let flValInput = createElement('input', { className: 'flm-input' });
             args.target.appendChild(flValInput);
-            dropInstance = new DropDownList({
+            args.target.parentElement.getElementsByClassName("e-flm_optrdiv")[0].remove();
+            args.target.parentElement.getElementsByClassName(
+              "e-flmenu-valuediv"
+            )[0].style.paddingTop = 0;
+            dropInstance = new MultiSelect({
               dataSource: new DataManager(fEmployeeData),
               fields: { text: 'FirstName', value: 'EmployeeID' },
               placeholder: 'Select a value',
-              popupHeight: '200px'
+              popupHeight: '200px',
+              mode: 'CheckBox',
+              filterType: 'Equal',
             });
             dropInstance.appendTo(flValInput);
           },
@@ -47,6 +56,14 @@ export default {
         },
       },
     };
+  },
+  methods: {
+    handleDataStateChange(state) {
+      console.log('handleDataStateChange triggered with state:', state);
+      console.log('pageState?.where', state?.where);
+      // returning the same data as before
+      this.data = { ...this.data };
+    },
   },
   provide: {
     grid: [Filter, ForeignKey, Edit, Toolbar],
