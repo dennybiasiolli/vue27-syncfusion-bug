9655dd3eb33c0c8f3ee5e9b145cce7a4b182942d
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-02-21 12:10:21 +0100

    GridExample: displaying the 'where' arg

--- src/components/GridExample.vue
+++ src/components/GridExample.vue
@@ -1,13 +1,17 @@
 <template>
-  <ejs-grid ref='grid' :dataSource='data' :allowFiltering='true' :filterSettings='filteroption' height='270px' :dataStateChange="handleDataStateChange">
-    <e-columns>
-      <e-column field='OrderID' headerText='Order ID' textAlign='Right' width=100></e-column>
-      <e-column field='EmployeeID' headerText='Employee Name' :dataSource='employeeData' foreignKeyValue='FirstName'
-        :filter='filter' width=120></e-column>
-      <e-column field='Freight' headerText='Freight' textAlign='Center' format='C2' width=80></e-column>
-      <e-column field='ShipCity' headerText='Ship City' width=130></e-column>
-    </e-columns>
-  </ejs-grid>
+  <div>
+    <ejs-grid ref='grid' :dataSource='data' :allowFiltering='true' :filterSettings='filteroption' height='270px' :dataStateChange="handleDataStateChange">
+      <e-columns>
+        <e-column field='OrderID' headerText='Order ID' textAlign='Right' width=100></e-column>
+        <e-column field='EmployeeID' headerText='Employee Name' :dataSource='employeeData' foreignKeyValue='FirstName'
+          :filter='filter' width=120></e-column>
+        <e-column field='Freight' headerText='Freight' textAlign='Center' format='C2' width=80></e-column>
+        <e-column field='ShipCity' headerText='Ship City' width=130></e-column>
+      </e-columns>
+    </ejs-grid>
+    <h3>Last dataStateChange args.where</h3>
+    <pre><code>{{ lastDataStateChangeArgs.where }}</code></pre>
+  </div>
 </template>
 
 <script>
@@ -25,6 +29,7 @@ let arr = [];
 export default {
   data: () => {
     return {
+      lastDataStateChangeArgs: {},
       data: { count: 123, result: data },
       employeeData: fEmployeeData,
       filteroption: { type: 'Menu' },
@@ -62,6 +67,7 @@ export default {
   },
   methods: {
     handleDataStateChange(state) {
+      this.lastDataStateChangeArgs = state;
       console.log('handleDataStateChange triggered with state:', state);
       console.log('pageState?.where', state?.where);
       // returning the same data as before
