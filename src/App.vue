5ad3be453558048bd6c50b2ad0bca4493abb5895
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2024-02-08 21:14:21 +0100

    adding sample code with column chooser bug

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,47 @@
 <template>
   <div id="app">
-    <div class="center-stuff">
+    <!-- <div class="center-stuff">
       <img alt="Vue logo" src="./assets/logo.png">
       <HelloWorld msg="Welcome to Your Vue.js App"/>
-    </div>
-    <GridWithDetail />
-    <GridExample />
+    </div> -->
+    <!-- <GridWithDetail /> -->
+    <!-- <GridExample /> -->
     <!-- <RichTextEditor /> -->
+    <GridColumnSelector />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 // import RichTextEditor from './components/RichTextEditor.vue'
-import GridExample from './components/GridExample.vue'
-import GridWithDetail from './components/GridWithDetail.vue'
+// import GridExample from './components/GridExample.vue'
+// import GridWithDetail from './components/GridWithDetail.vue'
+import GridColumnSelector from './components/GridColumnSelector.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HelloWorld,
     // RichTextEditor,
-    GridExample,
-    GridWithDetail,
+    // GridExample,
+    // GridWithDetail,
+    GridColumnSelector,
   }
 }
 </script>
 
 <style scoped>
-@import '../node_modules/@syncfusion/ej2-base/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-calendars/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-dropdowns/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-lists/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-navigations/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
-@import '../node_modules/@syncfusion/ej2-vue-richtexteditor/styles/material.css';
-@import "../node_modules/@syncfusion/ej2-vue-grids/styles/material.css";
+@import '@syncfusion/ej2-base/styles/material.css';
+@import '@syncfusion/ej2-buttons/styles/material.css';
+@import '@syncfusion/ej2-calendars/styles/material.css';
+@import '@syncfusion/ej2-dropdowns/styles/material.css';
+@import '@syncfusion/ej2-inputs/styles/material.css';
+@import '@syncfusion/ej2-lists/styles/material.css';
+@import '@syncfusion/ej2-popups/styles/material.css';
+@import '@syncfusion/ej2-navigations/styles/material.css';
+@import '@syncfusion/ej2-splitbuttons/styles/material.css';
+@import '@syncfusion/ej2-vue-richtexteditor/styles/material.css';
+@import "@syncfusion/ej2-vue-grids/styles/material.css";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/GridColumnSelector.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="app">
+    <div>
+      Steps to reproduce the issue:
+      <ol>
+        <li>
+          <u>Set column "Order ID" as visible</u>
+          <br>
+          actionComplete(columnstate) event triggered correctly<br>
+          with "Order ID" changed to visible=true
+        </li>
+        <li>
+          <u>Set columns "Order ID" and "Customer Name" as not visible</u>
+          <br>
+          actionComplete(columnstate) event triggered with only<br>
+          with "Customer Name" changed to visible=false
+        </li>
+      </ol>
+    </div>
+    <ul v-if="columnstate">
+      Columnstate event, columns changed:
+      <li v-for="col in columnstate" :key="col.field">
+        Column {{ col.headerText }} changed to visible={{ col.visible }}
+      </li>
+    </ul>
+    <ejs-grid :dataSource="data" :show-column-chooser="true" :toolbar="toolbarOptions" :columns="columns"
+      :loading-indicator="{ indicatorType: 'Shimmer' }" :allow-text-wrap="true"
+      :selection-settings="{ checkboxOnly: true, enableSimpleMultiRowSelection: true }"
+      :action-complete="handleActionComplete" />
+  </div>
+</template>
+<script>
+import { ColumnChooser, Toolbar } from "@syncfusion/ej2-vue-grids";
+import { data } from './datasource.js';
+
+export default {
+  data() {
+    return {
+      data: data,
+      toolbarOptions: ['ColumnChooser'],
+      columns: [],
+      columnstate: null,
+    };
+  },
+  provide: {
+    grid: [ColumnChooser, Toolbar],
+  },
+  async mounted() {
+    this.columns = [
+      {
+        type: "checkbox",
+        width: 50,
+      },
+      {
+        field: "actions",
+        headerText: "Actions",
+        width: 150,
+        showInColumnChooser: false,
+        valueAccessor: () => "Actions",
+      },
+      {
+        field: "OrderID",
+        headerText: "Order ID",
+        width: 125,
+        visible: false,
+      },
+      {
+        field: "CustomerID",
+        headerText: "Customer Name",
+        width: 150,
+      },
+      {
+        field: "Freight",
+        headerText: "Freight",
+        width: 120,
+        format: "C2",
+        textAlign: "Left",
+      },
+      {
+        field: "ShipCountry",
+        headerText: "Ship Country",
+        visible: true,
+        width: 150,
+      },
+      {
+        field: "ShipCity",
+        headerText: "Ship City",
+        visible: false,
+        width: 150,
+      },
+    ];
+  },
+  methods: {
+    handleActionComplete(args) {
+      if (args.requestType === "columnstate") {
+        console.log(args.columns);
+        this.columnstate = args.columns.map(col => ({
+          field: col.field,
+          headerText: col.headerText,
+          visible: col.visible
+        }));
+      }
+    },
+  },
+}
+</script>
